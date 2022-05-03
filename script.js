47358e5f9195e16f2c54ec1d18ddf56d1af6681c
James Macapagal <j.macapagal@gmail.com>
2022-05-03 12:09:35 -0500

    added div to .js file, added class properties to hero-image class in .css, added class hero-image to .html

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+const divA = document.createElement('div') //create divA
+divA.id = 'a'
+
+document.querySelector("body").appendChild(divA); //append divA to body
+
 const h1 = document.createElement("h1");
+h1.id = 'h1'
 h1.textContent = "Cece and Charlie: A Song of Cat Slaps and Kisses";
 
-document.querySelector("body").appendChild(h1);
+divA.append(h1) //append h1 to divA
+
+const divB = document.createElement('div')
+divB.id = 'b'
+
+document.querySelector("body").appendChild(divB); //append divB to body
 
 const h2 = document.createElement("h2");
+h2.id = 'h2'
 h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+divB.append(h2) //append h1 to divA
 
