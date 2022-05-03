b9d4e6d42aeab1ea1b7317904cccd3c255cceb41
James Macapagal <j.macapagal@gmail.com>
2022-05-03 11:24:31 -0500

    removed text from index.html, move text to script.js, added new h-tag characters to style.css

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+const h1 = document.createElement("h1");
+h1.textContent = "Cece and Charlie: A Song of Cat Slaps and Kisses";
+
+document.querySelector("body").appendChild(h1);
+
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
