f5dc954dedca41cc6cafee9e50de34b7ac4db5dc
James Macapagal <j.macapagal@gmail.com>
2022-05-03 12:54:49 -0500

    fixed figure, figcaption, and added to divImg, updated figure and figcaption css

--- script.js
+++ script.js
@@ -1,54 +1,57 @@
-//create divA
-const divA = document.createElement('div') 
-divA.id = 'a'
+//create div
+const divA = document.createElement('div') ;
+divA.id = 'a';
 
 //append divA to body
 document.querySelector("body").appendChild(divA); 
 
 //create h1 tag
 const h1 = document.createElement("h1");
-h1.id = 'h1'
+h1.id = 'h1';
 h1.textContent = "Cece and Charlie: A Song of Cat Slaps and Kisses";
 
 //append h1 to divA
-divA.append(h1) 
+divA.append(h1);
 
 //create image div
-const divImg = document.createElement('div')
-divImg.id = 'image'
-
-//create figure element
-const figure = document.createElement('figure')
-figure.className = "image"
+const divImg = document.createElement('div');
+divImg.id = 'image';
 
 //create img element
 const img = document.createElement('img');
 img.src = 'https://i.ibb.co/YTXJsKQ/Cece-and-Charlie.png';
-img.alt = "Cece and Charlie"
-img.id = "cece-and-charlie"
+img.alt = "Cece and Charlie";
+img.id = "cece-and-charlie";
+
+//create figure element
+const figure = document.createElement('figure');
 
 //add caption
-figure.figcaption = "Cece (left) and Charlie (right)";
+const figcaption = document.createElement('figcaption');
+figcaption.textContent = "Cece (left) and Charlie (right)";
 
-//append image to figure
+//append img to figure
 figure.appendChild(img);
 
+//append figcaption to figure
+figure.appendChild(figcaption)
+
 //append figure to div 
-divImg.append(figure)
+divImg.append(figure);
 
 //append div to body
-document.querySelector("body").appendChild(divImg)
+document.querySelector("body").appendChild(divImg);
 
 //create divB
-const divB = document.createElement('div')
+const divB = document.createElement('div');
 divB.id = 'b'
 //append divB to body
 document.querySelector("body").appendChild(divB); 
 
 //create h2 tag
 const h2 = document.createElement("h2");
-h2.id = 'h2'
+h2.id = 'h2';
 h2.textContent = "Cece and Charlie are my two cats. They have a complicated relationship.";
 
-divB.append(h2) //append h2 to divB
-
+ //append h2 to divB
+divB.append(h2);
